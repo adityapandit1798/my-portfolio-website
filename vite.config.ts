65ee288a13ffe846fdc701628cdd24bbc3f924f9
Aditya Pandit <pandit.aditya1798@gmail.com>
2025-07-12 20:16:16 +0530

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
@@ -9,7 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/my-portfolio-website/", // <-- Your GitHub repo name here
+  base: "/",               // 🤖 Serve from root
   build: {
     outDir: "dist",
   },
